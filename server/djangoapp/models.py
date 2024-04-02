4d49a78ee10c28128d39d52f3cbdd5cf6df7a793
LinaB <linauribazo25@gmail.com>
2024-04-02 19:44:26 -0400

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -13,6 +13,13 @@
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=100)
+    description = models.TextField()
+   
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+            "Description: " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -23,3 +30,28 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('COUPE', 'Coupe'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('HATCHBACK', 'Hatchback'),
+        ('WAGON', 'Wagon'),
+        ('MINIVAN', 'Minivan')  
+    ]
+
+    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=100)
+    type = models.CharField(null=False, max_length=20, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023, validators=[
+            MinValueValidator(2015, message='The year must be greater or equal than 2015'),
+            MaxValueValidator(2023, message='The year must be less or equal than 2023')
+        ])
+
+    def __str__(self):
+        return "Name: " + self.name + ", " + \
+        "Type: " + self.type + ", " + \
+        "Year: " + str(self.year)
