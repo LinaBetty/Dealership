69c02f01516e013bd469cec56fbc462b7f65fbd8
LinaBetty <153778937+LinaBetty@users.noreply.github.com>
2024-04-09 18:28:59 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -26,12 +26,12 @@ class CarModel(models.Model):
 
     car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
     name = models.CharField(null=False, max_length=100)
-    type = models.CharField(null=False, max_length=20, 
-                            choices = CAR_TYPES, default = 'SUV')
-    year = models.IntegerField(default = 2023, validators = [
-            MinValueValidator(2015, message = 'The year must be' + 
+    type = models.CharField(null=False, max_length=20,
+                            choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023, validators= [
+            MinValueValidator(2015, message='The year must be ' +
                               'greater or equal than 2015'),
-            MaxValueValidator(2023, message = 'The year must be ' + 
+            MaxValueValidator(2023, message='The year must be ' +
                               'less or equal than 2023')
         ])
 
